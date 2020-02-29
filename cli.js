6df2b7aeaee00ae63d1d4811f89a62ea8871602b
Brittni Lighthizer <brittnikirsten@gmail.com>
2020-02-28 19:52:41 -0600

    still need to finish the update employee part

--- cli.js
+++ cli.js
@@ -17,8 +17,8 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("Working!")
-    connection.end();
-    // runSearch();
+    // connection.end();
+    runSearch();
 });
 
 function runSearch() {
@@ -64,7 +64,8 @@ function runSearch() {
                     departmentAdd();
                     break;
 
-                case "UPDATE EMPLOYEE":
+                case "UPDATE EMPLOYEE ROLE":
+                    employeeUpdate();
                     break;
 
                 case "exit":
@@ -75,7 +76,7 @@ function runSearch() {
 }
 
 function employeeSearch() {
-    connection.query(query, function (err, res) {
+    connection.query("SELECT * FROM EMPLOYEE ", function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].first_name, res[i].last_name);
@@ -85,7 +86,7 @@ function employeeSearch() {
 }
 
 function roleSearch() {
-    connection.query(query, function (err, res) {
+    connection.query("SELECT * FROM role ", function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].title);
@@ -95,7 +96,7 @@ function roleSearch() {
 }
 
 function departmentSearch() {
-    connection.query(query, function (err, res) {
+    connection.query("SELECT * FROM department ", function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].name);
@@ -105,52 +106,89 @@ function departmentSearch() {
 }
 
 function departmentAdd() {
-    inquirer.prompt([
+    inquirer.prompt([{
         name: "name",
         message: "What is the department you'd like to add?"
-    ]).then(res => {
-        connection.query(query, function (err, res) {
-            "INSERT INTO department SET ?",
-            {
-                name: res.name,
-            }, function (err, res) {
-                if (err) throw err
-                departmentSearch();
-            }
+    }]).then(res => {
+        connection.query("INSERT INTO department SET ? ", {
+            name: res.name,
+        }, function (err, res) {
+            if (err) throw err
+            departmentSearch();
         });
     })
 };
 
 function roleAdd() {
-    inquirer.prompt([
-        name: "name",
+    inquirer.prompt([{
+        name: "title",
         message: "What is the role you'd like to add?"
+    },
+    {
+        name: "salary",
+        message: "What is the salary for this role?"
+    },
+    {
+        name: "departmentid",
+        message: "What is the department ID of this role?"
+    }
     ]).then(res => {
-        connection.query(query, function (err, res) {
-            "INSERT INTO role SET ?",
-            {
-                name: res.name,
-            }, function (err, res) {
-                if (err) throw err
-                roleSearch();
-            }
+        connection.query("INSERT INTO role SET ?", {
+            title: res.title,
+            salary: res.salary,
+            department_id: res.departmentid
+        }, function (err, res) {
+            if (err) throw err
+            roleSearch();
         });
     })
 };
 
 function employeeAdd() {
-    inquirer.prompt([
-        name: "name",
-        message: "What is the name of the employee you'd like to add?"
+    inquirer.prompt([{
+        name: "first_name",
+        message: "What is the frist name of the employee you'd like to add?"
+    }, {
+
+        name: "last_name",
+        message: "What is the last name of the employee you'd like to add?"
+    }, {
+
+        name: "role_id",
+        message: "What is the role id of the employee you'd like to add?"
+    }, {
+        name: "manager_id",
+        message: "What is the manager id assigned to this employee?"
+    }
     ]).then(res => {
-        connection.query(query, function (err, res) {
-            "INSERT INTO employee SET ?",
-            {
-                name: res.name,
-            }, function (err, res) {
-                if (err) throw err
-                employeeSearch();
-            }
-        });
+        connection.query("INSERT INTO employee SET ?", {
+            first_name: res.first_name,
+            last_name: res.last_name,
+            role_id: res.role_id,
+            manager_id: res.manager_id
+        }, function (err, res) {
+            if (err) throw err
+            employeeSearch();
+        }
+        );
     })
 };
+
+function employeeUpdate() {
+    console.log("Heyyy");
+    inquirer.prompt([{
+        name: "first_name",
+        message: "What is the first name of the employee you would like to update?"
+    }, {
+        name: "manager_id",
+        message: "Any updates to the manager id of the employee"
+    }
+    ]).then(res => {
+        // connection.query("SELECT * FROM EMPLOYEE WHERE first_name = ?", {
+        //     first_name: res.first_name
+        // }, function (err, res) {
+            // console.log(res);
+        // }
+        // );
+    })
+}
